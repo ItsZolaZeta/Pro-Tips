58ff333d0610d13be498f781c65b0b6ee563068f
Tatiana Tuor <tatianatuor@gmail.com>
2021-09-24 09:26:34 +0200

    add .png extension to avatar asset

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -18,7 +18,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def default_url(*args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    ActionController::Base.helpers.asset_path("avatar")
+    ActionController::Base.helpers.asset_path("avatar.png")
   
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
